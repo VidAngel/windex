cbed21b5ea37c884a5566e3ff7c6d35e40f9ab5b
Ian Hunter <ian@ianhunter.me>
2020-07-02 20:04:54 -0400

    Start using erlexec instead.

--- mix.exs
+++ mix.exs
@@ -14,14 +14,14 @@ defmodule Windex.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :erlexec]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:muontrap, "== 0.6.0"},
+      {:erlexec, "~> 1.17.0", override: true},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
