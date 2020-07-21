692caf0a2901d8210aac5377e13e036148fb4425
Ian Hunter <ian@ianhunter.me>
2020-07-21 00:51:15 -0400

    Use HMAC for validating requests

--- lib/http.ex
+++ lib/http.ex
@@ -2,6 +2,9 @@ defmodule Windex.HTTP do
   require Record
   Record.defrecordp :httpd, Record.extract(:mod, from_lib: "inets/include/httpd.hrl")
 
+  @hmac_key Application.get_env(:windex, :hmac_key, :crypto.strong_rand_bytes(32) |> Base.encode16)
+  @command_ttl Application.get_env(:windex, :command_ttl_seconds, 10*60)
+
   def unquote(:do)(req) do
     case httpd(req, :method) do
       'GET' -> do_get(req)
@@ -12,10 +15,15 @@ defmodule Windex.HTTP do
 
   defp do_post(req) do
     form = req |> httpd(:entity_body) |> :httpd.parse_query |> Map.new
-    command_index = form['command'] |> List.to_integer
-    command = Windex.available_opts |> Enum.at(command_index)
+    command = validate!(form['id'])
     {port, password} = Windex.spawn_server(command)
-    {:break, response: {200, Windex.HTTP.Template.vnc(port, password) |> String.to_charlist}}
+    case httpd(req, :request_uri) do
+      '/run.json' ->
+          json = Jason.encode!(%{port: port, password: password})
+          response = [code: 200, content_type: 'application/json', content_length: "#{byte_size(json)}" |> String.to_charlist]
+          {:break, response: {response, json |> String.to_charlist}}
+      _ -> {:break, response: {200, Windex.HTTP.Template.vnc(port, password) |> String.to_charlist}}
+    end
   end
 
   defp do_get(req) do
@@ -32,7 +40,6 @@ defmodule Windex.HTTP do
   defp is_root('/index.html'), do: true
   defp is_root(_), do: false
 
-  def nonce(), do: :crypto.strong_rand_bytes(10) |> Base.encode16
 
   def child_spec(_ignored_opts \\ []) do
     root = "#{:code.priv_dir(:windex)}/public" |> String.to_charlist
@@ -49,34 +56,44 @@ defmodule Windex.HTTP do
       start: {:inets, :start, args},
     }
   end
+
+  def command_id(term) do
+    encoded = term |> :erlang.term_to_binary |> Base.encode16
+    creation = DateTime.utc_now |> DateTime.to_unix
+    hmac = :crypto.hmac(:sha256, @hmac_key, "#{encoded}.#{creation}") |> Base.encode16
+    "#{encoded}.#{creation}.#{hmac}"
+  end
+
+  def validate!(id) do
+    IO.inspect(id)
+    [term, creation, hmac] = "#{id}" |> String.split(".")
+    IO.inspect(term)
+    now = DateTime.utc_now |> DateTime.to_unix
+    creation = creation |> String.to_integer
+    true = (now - creation) < @command_ttl
+    ^hmac = :crypto.hmac(:sha256, @hmac_key, "#{term}.#{creation}") |> Base.encode16
+    term |> Base.decode16! |> :erlang.binary_to_term
+  end
 end
 
 defmodule Windex.HTTP.Template do
   require EEx
 
   def index(:json) do
-    {nonce, commands} = index()
-    index(nonce, commands) |> String.to_charlist
-    json = Jason.encode!(%{nonce: nonce, commands: Enum.map(commands, &inspect/1)})
+    json = Jason.encode!(commands())
     {:response, [code: 200, content_type: 'application/json', content_length: "#{byte_size(json)}" |> String.to_charlist], json |> String.to_charlist}
   end
 
   def index(:html) do
-    {nonce, commands} = index()
-    {200, index(nonce, commands) |> String.to_charlist}
+    {200, index(commands()) |> String.to_charlist}
   end
 
-  defp index() do
-    nonce = Windex.HTTP.nonce()
-    commands = Windex.available_opts()
-    Task.start(fn ->
-      Registry.register(Windex.OptionSets, nonce, commands)
-      Process.sleep(1_000*60*15)
-      Registry.unregister(Windex.OptionSets, nonce)
-    end)
-    {nonce, commands}
+  defp commands() do
+    IO.inspect(Windex.available_opts() |> Enum.map(fn cmd ->
+      %{label: inspect(cmd), id: Windex.HTTP.command_id(cmd)}
+    end))
   end
   
-  EEx.function_from_file(:defp, :index, "#{:code.priv_dir(:windex)}/index.eex", [:nonce, :commands])
-  EEx.function_from_file(:def,  :vnc,   "#{:code.priv_dir(:windex)}/vnc.eex",  [:port, :password])
+  EEx.function_from_file(:def, :index, "#{:code.priv_dir(:windex)}/index.eex", [:commands])
+  EEx.function_from_file(:def,  :vnc,  "#{:code.priv_dir(:windex)}/vnc.eex",  [:port, :password])
 end
